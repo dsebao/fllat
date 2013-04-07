63792dbe19af4d713416c86c8bfb6cfca6bf8a0e
Alfred Xing <xing@lfred.info>
2013-04-07 16:30:43 -0700

    Update Prequel

--- vendor/prequel.php
+++ vendor/prequel.php
@@ -1,13 +1,15 @@
 <?php
 
 /**
- * Prequel: SQL-based manipulations for PHP arrays. Select. Where. Like. Union. Join.
+ * Prequel: SQL-based manipulations for PHP arrays.
+ * Select. Where. Like. Union. Join.
  * 
- * Copyright MMXIII Alfred Xing http://alfredxing.com/
+ * PHP version 5
+ *
+ * @author    Alfred Xing <xing@lfred.info>
+ * @copyright 2013 Alfred Xing
+ * @license   LICENSE.md MIT License
  * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, and/or sublicense copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 class Prequel
@@ -15,10 +17,14 @@ class Prequel
 
 	/**
 	 * Get a set of columns for all rows
-	 * @param  array $cols  the list of columns to get, empty for all
+	 * 
+	 * @param array $cols the list of columns to get, empty for all
+	 * @param array $db   the array to select from
+	 * 
 	 * @return array
 	 */
-	function select($cols = array(), $db) {
+	function select($cols = array(), $db)
+	{
 		$_result = array();
 		$_values = array();
 		if ($cols === array()) {
@@ -26,7 +32,7 @@ class Prequel
 				foreach (array_keys($row) as $c) {
 					$_values[$c] = $row[$c];
 				};
-				if (values)
+				if ($_values)
 					$_result[] = $values;
 				$_values = array();
 			}

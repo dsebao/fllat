bff2c1c59987712169f95b1e85fd9043c346aacc
Alfred Xing <xing@lfred.info>
2013-04-07 10:53:45 -0700

    Change initialization and add `join`

--- fllat.php
+++ fllat.php
@@ -10,19 +10,22 @@ class Fllat
 	function __construct($name, $path = "db") {
 		$this -> name = $name;
 		$this -> path = $path;
-		$this -> file = realpath($path . "/" . $this -> name . '.dat');
+		$this -> go($path . "/" . $this -> name . '.dat');
 	}
 
 	/**
-	 * Initialize database for work and returns database status
-	 * @return string
+	 * Initialize database for work
+	 * @param  string $file  relative path of database
+	 * @return string        existence status of database
 	 */
-	function go() {
-		if (file_exists($this -> file)) {
+	function go($file) {
+		if (file_exists($file)) {
+			$this -> file = realpath($file);
 			return "Database '".$this -> name."' already exists.";
 		}
 		else {
-			file_put_contents($this -> file, "");
+			file_put_contents($file, "");
+			$this -> file = realpath($file);
 			return "Database '".$this -> name."' successfully created.";
 		}
 	}
@@ -75,7 +78,7 @@ class Fllat
 	function get($col,$key,$val) {
 		$old = file_get_contents($this -> file);
 		if ($old) {
-			$db = json_decode(file_get_contents($this -> file),true);
+			$db = json_decode($old, true);
 			foreach ($db as $row) {
 				if ($row[$key] === $val && $row[$col]) {
 					return $row[$col];
@@ -97,7 +100,7 @@ class Fllat
 	function exists($key,$val) {
 		$old = file_get_contents($this -> file);
 		if ($old) {
-			$db = json_decode(file_get_contents($this -> file),true);
+			$db = json_decode($old, true);
 			$result = false;
 			foreach ($db as $row) {
 				if ($row[$key] === $val) {
@@ -113,13 +116,13 @@ class Fllat
 
 	/**
 	 * Get a set of columns for all rows
-	 * @param  array $cols the list of columns to get, empty for all
+	 * @param  array $cols  the list of columns to get, empty for all
 	 * @return array
 	 */
-	function select($cols) {
+	function select($cols = array()) {
 		$old = file_get_contents($this -> file);
 		if ($old) {
-			$db = json_decode(file_get_contents($this -> file),true);
+			$db = json_decode($old, true);
 			$result = array();
 			$values = array();
 			if ($cols === array()) {
@@ -127,15 +130,18 @@ class Fllat
 					foreach (array_keys($row) as $c) {
 						$values[$c] = $row[$c];
 					};
-					$result[] = $values;
+					if (values)
+						$result[] = $values;
 					$values = array();;
 				}
 			} else {
 				foreach ($db as $row) {
 					foreach ((array) $cols as $c) {
-						$values[$c] = $row[$c];
+						if ($row[$c])
+							$values[$c] = $row[$c];
 					};
-					$result[] = $values;
+					if ($values)
+						$result[] = $values;
 					$values = array();
 				}
 			}
@@ -155,7 +161,7 @@ class Fllat
 	function where($cols,$key,$val) {
 		$old = file_get_contents($this -> file);
 		if ($old) {
-			$db = json_decode(file_get_contents($this -> file),true);
+			$db = json_decode($old, true);
 			$result = array();
 			$values = array();
 			if ($cols === array()) {
@@ -196,7 +202,7 @@ class Fllat
 	function in($cols, $key, $val) {
 		$old = file_get_contents($this -> file);
 		if ($old) {
-			$db = json_decode(file_get_contents($this -> file),true);
+			$db = json_decode($old, true);
 			$result = array();
 			$values = array();
 			if ($cols === array()) {
@@ -227,10 +233,17 @@ class Fllat
 		}
 	}
 
+	/**
+	 * Matches keys and values based on a regular expression
+	 * @param  array  $cols   the columns to return; an empty array returns all columns
+	 * @param  string $key    the column whose value to match
+	 * @param  string $regex  the regular expression to match
+	 * @return array
+	 */
 	function like($cols,$key,$regex) {
 		$old = file_get_contents($this -> file);
 		if ($old) {
-			$db = json_decode(file_get_contents($this -> file),true);
+			$db = json_decode($old, true);
 			$result = array();
 			$values = array();
 			if ($cols === array()) {
@@ -259,6 +272,112 @@ class Fllat
 		else {
 			return ;
 		}
+	}
+
+	/**
+	 * Merges two databases and gets rid of duplicates
+	 * @param  array $cols    the columns to merge
+	 * @param  Fllat $second  the second database to merge
+	 * @return array          the merged array
+	 */
+	function union($cols, $second) {
+		return array_map("unserialize", array_unique(array_map("serialize",
+			array_merge(
+				$this -> select($cols),
+				$second -> select($cols)
+				))
+		));
+	}
+
+
+	function join($method, $cols, $second, $match) {
+		$_left = file_get_contents($this -> file);
+		$_right = file_get_contents($second -> file);
+		if ($_left && $_right) {
+			$_left = json_decode($_left ,true);
+			$_right = json_decode($_right ,true);
+			$_result = array();
+			$_values = array();
+			if ($method === "inner") {
+				foreach ($_left as $lrow) {
+					foreach ($_right as $rrow) {
+						if ($lrow[$match[0]] === $rrow[$match[1]]) {
+							$_result[] = array_merge($lrow, $rrow);
+						}
+					}
+				}
+			} elseif ($method === "left") {
+				foreach ($_left as $lrow) {
+					foreach ($_right as $rrow) {
+						if ($lrow[$match[0]] === $rrow[$match[1]]) {
+							$_values = array_merge($lrow, $rrow);
+							break;
+						} else {
+							$_values = $lrow;
+						}
+					}
+					$_result[] = $_values;
+					$_values = array();
+				}
+			} elseif ($method === "right") {
+				foreach ($_left as $lrow) {
+					foreach ($_right as $rrow) {
+						if ($lrow[$match[0]] === $rrow[$match[1]]) {
+							$_values = array_merge($lrow, $rrow);
+							break;
+						} else {
+							$_values = $rrow;
+						}
+					}
+					$_result[] = $_values;
+					$_values = array();
+				}
+			}
+			elseif ($method === "full") {
+				$_result = array_map("unserialize", array_unique(array_map("serialize",
+					array_merge(
+						$this -> join("left", $cols, $second, $match),
+						$this -> join("right", $cols, $second, $match)
+						))
+				));
+			}
+			return $_result;
+		}
+		else {
+			return ;
+		}
+	}
+
+	/**
+	 * Counts the number of items per column or for all columns
+	 * @param  string $col  the column name to count. No input counts all columns.
+	 * @return int          the number of rows containing that column.
+	 */
+	function count($col = "") {
+		if ($col === "") {
+			$query = array();
+		} else {
+			$query = (array) $col;
+		}
+		return count($this -> select($query));
+	}
+
+	/**
+	 * Gets the first item of a column
+	 * @param  string $col  the column to look at
+	 * @return mixed        the first item in the column
+	 */
+	function first($col) {
+		return $this -> select((array) $col)[0][$col];
+	}
+
+	/**
+	 * Gets the last item in a column
+	 * @param  string $col the name of the column to look at
+	 * @return mixed       the last item in the column
+	 */
+	function last($col) {
+		return end($this -> select((array) $col))[$col];
 	}
 
 }
